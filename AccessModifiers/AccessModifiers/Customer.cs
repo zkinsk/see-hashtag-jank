518b23eb0da1f63a7cd66c71ff1deded9be27a7d
Scott Zinski <szinski@roll-call.com>
2022-11-20 19:33:38 -0500

    Access modifiers

---
+++ AccessModifiers/AccessModifiers/Customer.cs
@@ -0,0 +1,18 @@
+﻿namespace AccessModifiers;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public void Promote()
+    {
+        var rating = CalculateRating();
+        Console.WriteLine($"Promoted: {Name} {Id} - {rating}");
+    }
+
+    private int CalculateRating()
+    {
+        return 1;
+    }
+}
---
+++ AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace AccessModifiers;
+
+public class GoldCustomer : Customer
+{
+    public void OfferVoucher()
+    {
+        Console.WriteLine("Voucher Offered");
+    }
+}
+
+public static class Program
+{
+    private static void Main()
+    {
+        var customer = new Customer();
+        customer.Promote();
+
+        var goldCustomer = new GoldCustomer();
+        goldCustomer.OfferVoucher();
+    }
+}
